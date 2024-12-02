8cab9e42edd3444679094633eee6efd42366b127
haytam <haytam.hadad.2022@gmail.com>
2024-12-02 17:43:27 +0100

    .

--- db.js
+++ db.js
@@ -1,16 +1,20 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
-// Create a new pool instance with database configuration
+
+// const pool = new Pool({
+//   user: 'postgres',     
+//   host: 'localhost',    
+//   database: 'pfe',     
+//   password: '',      
+//   port: 5432,          
+// });
+
 const pool = new Pool({
-  user: 'postgres',       // Your PostgreSQL username
-  host: 'localhost',      // Database server host
-  database: 'pfe',        // Name of your database
-  password: '4321',       // Your PostgreSQL password
-  port: 5432,             // Default PostgreSQL port
+  connectionString: process.env.DATABASE_URL
 });
 
-// Function to execute queries with parameters
+
 export const query = async (text, params) => {
   try {
     const result = await pool.query(text, params);
